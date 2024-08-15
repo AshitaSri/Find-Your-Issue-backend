550b3a5d489ac9ddee86add732c7398b47b6f66f
Ashita <108900826+AshitaSri@users.noreply.github.com>
2024-08-15 23:14:11 +0530

    added cors

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const homeRouter = require('./home');
 
+
 const app = express();
+const corsOptions = {
+    origin: 'https://find-your-issue.vercel.app',
+    optionsSuccessStatus: 200
+  };
+  
+  app.use(cors(corsOptions));
+
 
 app.use('/', homeRouter);
 
